96d6727de12ba17ec7cfcd3ae2a3c1575549b2bd
KateKhukhka <khukhka@mail.ru>
2024-11-18 15:37:06 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const image = document.getElementById("gallery__img");
+function showImage1() {
+  image.src = "./img/grey_cat.jpg";
+}
+function showImage2() {
+  image.src = "./img/black_cat.jpg";
+}
